162c5025481a384ab05b00c5f9134f2def6e4c3e
yashkvr <yashincrypto5@gmail.com>
2023-09-03 19:09:19 +0530

    added framer motion

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+"use client"
+import React from 'react';
+import { motion } from "framer-motion";
+import { Link } from 'react-scroll'
 
 const Hero = () => {
     return (
@@ -10,7 +13,12 @@ const Hero = () => {
                 <h2 className="text-3xl md:text-5xl font-bold mb-2 underline">Donation For Strays</h2>
                 <p className="text-lg lg:text-xl py-2 px-5 text-center">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nisi iure repellat animi quam necessitatibus, ut eaque. Eum cum sapiente quas voluptates veritatis nostrum, in neque itaque quaerat quasi animi.</p>
-                <button className="text-xl font-semibold bg-[#950101] text-white px-2 py-1 rounded-lg hover:bg-[#FF0000] m-3">Donate Now</button>
+                <Link to="donation" smooth={true} duration={500}>
+                    <motion.button
+                        whileHover={{ scale: 1.1 }}
+                        whileTap={{ scale: 0.9 }}
+                        className="text-xl font-semibold bg-[#950101] text-white px-2 py-1 rounded-lg hover:bg-[#FF0000] m-3">Donate Now</motion.button>
+                </Link>
             </div>
         </div>
     )
--- src/app/components/Mission.jsx
+++ src/app/components/Mission.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const Mission = () => {
     return (
@@ -6,16 +6,16 @@ const Mission = () => {
             <div className="lg:w-[60vw] lg:h-[60vh] flex flex-col items-center justify-between">
                 <h2 className="text-5xl font-bold mb-2 text-start underline">Our Mission</h2>
                 <div className="flex lg:w-[30vw] w-[80vw] items-center justify-center lg:justify-start p-2 m-2">
-                    <img src="https://imgur.com/dtgDYk1.png" alt="" className=" h-10 lg:h-20" />
+                    <img src="https://imgur.com/1r9JA2b.png" alt="" className=" h-10 lg:h-20" />
                     <p className="text-md md:text-lg lg:text-2xl font-semibold m-2">Provide food for strays</p>
                 </div>
                 <div className="flex lg:w-[30vw] w-[80vw] items-center justify-center lg:justify-start p-2 m-2">
-                    <img src="https://imgur.com/dtgDYk1.png" alt="" className=" h-10 lg:h-20" />
-                    <p className="text-md md:text-lg lg:text-2xl font-semibold m-2">Provide food for strays</p>
+                    <img src="https://imgur.com/w50cIQI.png" alt="" className=" h-10 lg:h-20" />
+                    <p className="text-md md:text-lg lg:text-2xl font-semibold m-2">Vaccinations and Shelter</p>
                 </div>
                 <div className="flex lg:w-[30vw] w-[80vw] items-center justify-center lg:justify-start p-2 m-2">
-                    <img src="https://imgur.com/dtgDYk1.png" alt="" className=" h-10 lg:h-20" />
-                    <p className="text-md md:text-lg lg:text-2xl font-semibold m-2">Provide food for strays</p>
+                    <img src="https://imgur.com/VZW6a1Z.png" alt="" className=" h-10 lg:h-20" />
+                    <p className="text-md md:text-lg lg:text-2xl font-semibold m-2">Lots of Love</p>
                 </div>
             </div>
             <div className="p-5 lg:w-[40vw]">
