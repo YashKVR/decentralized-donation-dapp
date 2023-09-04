c7de4bde0ed94b72f79ed934579ef48594d5411f
yashkvr <yashincrypto5@gmail.com>
2023-09-04 12:02:40 +0530

    little tweeks

--- src/app/components/Donation.jsx
+++ src/app/components/Donation.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Donation = () => {
     return (
-        <div name='donation' className="flex items-center justify-center w-full h-screen">Donation</div>
+        <div name='donation' className="flex flex-col items-center w-full h-screen">
+            <h2 className="text-3xl md:text-5xl font-bold mb-2 underline pt-5">Donation</h2>
+            <div className="border-2 border-red-500 flex flex-col lg:flex-row">
+                <div className="border-2 border-red-500 flex flex-col items-center lg:w-[30vw] h-[30vh]">
+                    hello
+                </div>
+
+
+            </div>
+        </div>
     )
 }
 
